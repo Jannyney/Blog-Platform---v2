b6b612d398aff3f287fd1ddd98f2350ce3745e8c
Janny Orlando <57592173+Jannyney@users.noreply.github.com>
2023-10-16 13:42:07 +0700

    add

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,10 @@
+<div class="hero min-h-screen" style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Welcome to <br>Blog Platform</h1>
+      <p class="mb-5">Here, you can embark on a remarkable adventure of crafting engaging narratives, thought-provoking articles, and eye-catching blog posts that captivate your audience and leave a lasting digital footprint. </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Hero from "$lib/Hero.svelte";
+    import ShowBlog from "$lib/ShowBlog.svelte";
+    import PopularBlog from "$lib/PopularBlog.svelte";
+    import Stat from "$lib/Stat.svelte";
+</script>
+
+
+<Hero/>
+<PopularBlog/>
+<ShowBlog/>
+<Stat/>
+
